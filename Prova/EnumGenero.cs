5811c0cd995ba266255d8210d8e9f4a5bdc15a07
Silvano <silvanode.jesus@hotmail.com>
2021-04-14 12:05:35 -0300

    prova

--- Prova/EnumGenero.cs
+++ Prova/EnumGenero.cs
@@ -1,17 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
 
-    public class EnumGenero
+    class EnumGenero
     {
         
-        public Class()
-        {
-            
-        }
+        enum Genero {Ficção, Informatica, Games, Nefócios};
 
     }
 
